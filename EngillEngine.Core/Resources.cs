69bed7501653de68108b11603f51e37f57093d6a
jocmgbej <joc.mgbejume@student.avans.nl>
2018-04-23 15:25:05 +0200

    initial commit

---
+++ EngillEngine.Core/Resources.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace EngillEngine.Core
+{
+    public class Resources
+    {
+		/// <summary>
+        /// The instance.
+        /// </summary>
+		public static Resources instance;
+
+        /// <summary>
+        /// Initializes the resources.
+        /// </summary>
+        /// <param name="contentManager">Content manager.</param>
+        /// <param name="graphicsDeviceManager">Graphics device manager.</param>
+        /// <param name="gameWindow">Game window.</param>
+		public static void InitializeResources(ContentManager contentManager, GraphicsDeviceManager graphicsDeviceManager, GameWindow gameWindow)
+		{
+			
+		}
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+		public static Resources GetInstance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+        /// <summary>
+        /// Gets the content manager.
+        /// </summary>
+        /// <value>The content manager.</value>
+		public static ContentManager ContentManager
+		{
+			get
+			{
+				return instance.contentManager;
+			}
+		}
+
+        /// <summary>
+        /// Gets the graphics device manager.
+        /// </summary>
+        /// <value>The graphics device manager.</value>
+		public static GraphicsDeviceManager GraphicsDeviceManager
+		{
+			get
+			{
+				return instance.graphicsDeviceManager;
+			}
+		}
+
+        /// <summary>
+        /// Gets the graphics device.
+        /// </summary>
+        /// <value>The graphics device.</value>
+		public static GraphicsDevice GraphicsDevice 
+		{
+			get
+			{
+				return instance.graphicsDeviceManager.GraphicsDevice;
+			}
+		}
+                
+		private ContentManager contentManager;
+		private GraphicsDeviceManager graphicsDeviceManager;
+
+		public Resources(ContentManager contentManager, GraphicsDeviceManager graphicsDeviceManager, GameWindow gameWindow)
+        {
+			this.contentManager = contentManager;
+			this.graphicsDeviceManager = graphicsDeviceManager;
+        }
+    }
+}
---
+++ EngillEngine.Core/Util/IWindow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace EngillEngine.Core.Util
+{
+	public enum WindowStates
+	{
+		WINDOWED,
+		BORDERLESS,
+        FULLSCREEN
+	}
+
+	public delegate void OnWindowResize(Point size);
+	public delegate void OnWindowStateChange(WindowStates newState);
+
+    public interface Window
+    {        
+		event OnWindowResize OnWindowResize
+		{
+			add;
+			remove;
+		}
+
+		event OnWindowStateChange OnWindowStateChange
+		{
+			add;
+			remove;
+		}
+        
+		Point Size
+		{
+			get;
+			set;
+		}
+
+		WindowStates States
+		{
+			get;
+			set;
+		}
+
+		void CheckForEvents();
+    }
+
+}
